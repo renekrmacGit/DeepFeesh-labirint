d21b009e095f5283dfe1c73bccb3e094fa1a2921
renekrmacGit <rene.krmac@tscng.org>
2026-03-20 09:18:26 +0100

    Implement Easter egg for keydown event
    Add Easter egg that triggers alert with creator's name.

--- js/script.js
+++ js/script.js
@@ -284,3 +284,16 @@ document.getElementById('btnSolve').addEventListener('click', function() {
 // zagon na zacetku
 startGame();
 requestAnimationFrame(gameLoop);
+
+// ester egg
+let input = "";
+
+window.addEventListener('keydown', function(e) {
+    input += e.key; // dodamo pritisnjeno črko
+
+    // če to kar je napisal vsebuje tvoje ime, vržemo okence
+    if (input.includes("rene")) {
+        alert("Made by: Rene Krmac 😎");
+        input = ""; // pobrišemo spomin, da lahko spet napiše
+    }
+});
